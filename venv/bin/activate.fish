2b36551c6723d10339c373087c73f78fa955d332
Tais-MacBook-Pro <jacobpham82@gmail.com>
2021-03-22 23:45:38 -0700

    heroku-commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/taipham/Documents/FoxvProject/venv"
+set -gx VIRTUAL_ENV "/Users/taipham/Desktop/Projects/tai/FoxvProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
